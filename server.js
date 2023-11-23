c89db0b109777082c63379eae770fe8e3d226270
Eric_24 <eric_24@Eric-24s-MacBook-Pro.local>
2023-11-23 15:22:42 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,117 @@
+const express = require("express");
+const cors = require("cors");
+const knex = require("knex");
+const bcrypt = require("bcrypt");
+
+const db = knex({
+  client: "pg",
+  connection: {
+    host: "127.0.0.1",
+    port: 5432,
+    user: "eric_24",
+    password: "manchester24",
+    database: "smart-brain",
+  },
+});
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  const message = {
+    message: "Hello, world",
+  };
+
+  res.json(message);
+});
+
+app.post("/signin", async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const isUser = await db
+      .select("email", "hash")
+      .from("login")
+      .where("email", "=", email);
+    if (isUser) {
+      const isValidPass = await bcrypt.compare(password, isUser[0].hash);
+      if (isValidPass) {
+        const user = await db
+          .select("*")
+          .from("users")
+          .where("email", "=", email);
+
+        res.status(200).json(user);
+      } else {
+        res.status(400).json("Wrong credentials");
+      }
+    }
+  } catch (error) {
+    res.status(400).json("unable to signin, check credentials");
+  }
+});
+
+app.post("/register", async (req, res) => {
+  const { username, email, password } = req.body;
+  const hash = await bcrypt.hash(password, 10);
+  db.transaction((trx) => {
+    trx
+      .insert({
+        hash: hash,
+        email: email,
+      })
+      .into("login")
+      .returning("email")
+      .then((loginEmail) => {
+        return trx("users")
+          .returning("*")
+          .insert({
+            email: loginEmail[0].email,
+            name: username,
+            joined: new Date(),
+          })
+          .then((user) => {
+            res.json(user);
+          });
+      })
+      .then(trx.commit)
+      .catch(trx.rollback);
+  }).catch((err) => res.status(400).json("unable to register"));
+});
+
+app.get("/profile/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const user = await db.select().from("users").where({
+      id: id,
+    });
+
+    if (user.length > 0) {
+      res.json(user[0]);
+    } else {
+      res.status(400).json("Error getting user");
+    }
+  } catch (error) {
+    res.status(400).json(error);
+  }
+});
+
+app.put("/image", async (req, res) => {
+  const { id } = req.body;
+  try {
+    const entries = await db("users")
+      .where("id", "=", id)
+      .increment("entries", 1)
+      .returning("entries");
+
+    res.json(entries[0]);
+  } catch (error) {
+    res.status(400).json("unable to get entries");
+  }
+});
+
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
